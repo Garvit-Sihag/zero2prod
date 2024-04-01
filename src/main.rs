767c72ac750a392053ef9ff5f7d885ce924c5189
Garvit-Sihag <garysihag@gmail.com>
2024-04-01 14:09:06 +0530

    add docker image for postgres and integration test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listner = TcpListener::bind("127.0.0.1:0").expect("Falied to bind the port");
+    let server = run(listner).expect("failed to start the application");
+
+    server.await
 }
---
+++ tests/health_check.rs
@@ -0,0 +1,72 @@
+use std::net::TcpListener;
+
+use reqwest::Client;
+// Launch our application in the background ~somehow~
+fn spawn_app() -> String {
+    let listner = TcpListener::bind("127.0.0.1:0").expect("failed to bind a random port");
+    let port = listner.local_addr().unwrap().port();
+    let server = zero2prod::run(listner).expect("Falied to launch application");
+    let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
+}
+
+#[tokio::test]
+async fn health_check_works() {
+    let address = spawn_app();
+
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get(&format!("{}/health_check", &address))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+#[tokio::test]
+async fn subscribe_returns_200_for_valid_form_data() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_400_for_missing_form_data() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_case = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (body, error_msg) in test_case {
+        let response = client
+            .post(&format!("{}/subscriptions", &address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_msg
+        );
+    }
+}
